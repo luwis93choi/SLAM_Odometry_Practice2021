3052f095696d84493875a0d9acef0b6ba2858dcb
luwis93choi <luwis93choi@hotmail.com>
2021-04-19 04:35:09 +0900

    Removed legacy code / Added comments

--- 01_Probabilistic_Odometry_without_Sensor.py
+++ 01_Probabilistic_Odometry_without_Sensor.py
@@ -7,7 +7,9 @@ discrete_grid = np.zeros((200, 200, 36), dtype=float)
 
 groundtruth_poses = []
 
+#######################
 ### Initial Setting ###
+#######################
 
 pose = [0, 0]
 groundtruth_poses.append(pose)
@@ -15,20 +17,20 @@ groundtruth_poses.append(pose)
 print('Initial pose : {}'.format(pose))
 plt.plot(pose[0], pose[1], 'x')
 
+##########################################
 ### Iterative Odometry Pose Estimation ###
-
-# Control Commands
-
-for iteration in range(50):
+##########################################
+for iteration in range(30):
 
     print('Iteration : {}'.format(iteration))
 
+    # At the first iteration, acquire the odometry distribution from initial pose
     if iteration == 0:
 
         # Control Commands
         dx = random.randint(low=10, high=220, size=1)[0]
-        # dy = random.randint(low=10, high=110, size=1)[0]
-        dy = 0
+        dy = random.randint(low=10, high=110, size=1)[0]
+        # dy = 0
         dtheta = random.uniform(low=-3.14, high=3.14, size=1)[0]
 
         pose = [pose[0] + dx, pose[1] + dy]
@@ -54,11 +56,12 @@ for iteration in range(50):
         print('Pose Est : [{}, {}]'.format(pose_est_x, pose_est_y))
         plt.plot(pose_est_x, pose_est_y, 'o')
 
+    # Iteratively acquire odometry distribution from each odometry estimation and accumulate it
     else:
-        # dx = random.randint(low=10, high=220, size=1)[0]
-        # dy = random.randint(low=10, high=110, size=1)[0]
-        dx += 100
-        dy = 0
+        dx = random.randint(low=10, high=220, size=1)[0]
+        dy = random.randint(low=10, high=110, size=1)[0]
+        # dx += 100
+        # dy = 0
         dtheta = random.uniform(low=-3.14, high=3.14, size=1)[0]
         
         pose = [pose[0] + dx, pose[1] + dy]
@@ -67,35 +70,27 @@ for iteration in range(50):
         translation = np.sqrt(dx**2 + dy**2)
         print('Translation : {}'.format(translation))
 
-        # mean = [pose_est_x + dx, pose_est_y + dy]
-        # cov = [[translation, 0], 
-        #        [0, translation]]
-        # odom_est = np.random.multivariate_normal(mean, cov, 5000).T
-
-        # sample_idx_mask = np.random.randint(low=0, high=odom_est.shape[1], size=100)
-
-        # sample_odom_est = odom_est[:, sample_idx_mask]
-
         ####################################################################################
+        # Gaussian Noise Accumulation
         idx = 0
         for sample_odom in zip(sample_odom_est.T):
 
-            # print('sample_odom : {} {}'.format(sample_odom[0][0], sample_odom[0][1]))
-
+            # For each noisy pose estimation, apply translation and sample Gaussian distriubtion.
+            # Covariance of the distribution depends on the value of translation.
+            # This is because longer the robot travels, its odometry will have more likelihood to receive noise.
             mean = [sample_odom[0][0] + dx, sample_odom[0][1] + dy]
             cov = [[translation, 0], 
                    [0, translation]]
             odom_est = np.random.multivariate_normal(mean, cov, 100).T
 
+            # Accumulate all the odometry distribution values
             if idx == 0:
                 concat_array = odom_est
                 idx += 1
-
             else:
                 concat_array = np.concatenate((concat_array, odom_est), axis=1)
 
-            # print(concat_array.shape)
-
+        # Since it is not possible to use all the values in odometry distribution, acquire a sample from the distribution
         sample_idx_mask = np.random.randint(low=0, high=concat_array.shape[1], size=500)
 
         sample_odom_est = concat_array[:, sample_idx_mask]
