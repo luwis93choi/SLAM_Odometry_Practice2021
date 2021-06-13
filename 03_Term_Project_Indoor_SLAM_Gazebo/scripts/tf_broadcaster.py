f3b8d34f15984a4e98f2ef6614a0f5291e33ab14
luwis93choi <luwis93choi@hotmail.com>
2021-06-13 13:50:40 +0900

    Added imu data subscriber for odom-baselink orientation update

--- 03_Term_Project_Indoor_SLAM_Gazebo/scripts/tf_broadcaster.py
+++ 03_Term_Project_Indoor_SLAM_Gazebo/scripts/tf_broadcaster.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import rospy
 
-# import tf_conversions
+from sensor_msgs.msg import Imu
+from gazebo_msgs.msg import LinkStates
 
 import tf2_ros
 import geometry_msgs.msg
@@ -10,9 +11,27 @@ tf_broadcaster = tf2_ros.TransformBroadcaster()
 tf_msgs_sensor2base = geometry_msgs.msg.TransformStamped()
 tf_msgs_base2odom = geometry_msgs.msg.TransformStamped()
 
+def imu_subscribe_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + '%s', data.orientation)
+
+    tf_msgs_base2odom.header.stamp = rospy.Time.now()
+    tf_msgs_base2odom.header.frame_id = 'odom'
+    tf_msgs_base2odom.child_frame_id = 'base_link'
+    tf_msgs_base2odom.transform.translation.x = 0
+    tf_msgs_base2odom.transform.translation.y = 0
+    tf_msgs_base2odom.transform.translation.z = 0
+    tf_msgs_base2odom.transform.rotation = data.orientation
+    tf_broadcaster.sendTransform(tf_msgs_base2odom)
+    
+def gazebo_model_pose_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + '%s', data.orientation)
+
 if __name__ == '__main__':
     rospy.init_node('tf_broadcaster')
     rate = rospy.Rate(30)
+
+    rospy.Subscriber('/imu/data', Imu, imu_subscribe_callback)
+    # rospy.Subscriber('/gazebo/link_states', LinkStates, )
 
     while not rospy.is_shutdown():
 
@@ -27,17 +46,5 @@ if __name__ == '__main__':
         tf_msgs_sensor2base.transform.rotation.z = 0
         tf_msgs_sensor2base.transform.rotation.w = 1
         tf_broadcaster.sendTransform(tf_msgs_sensor2base)
-
-        tf_msgs_base2odom.header.stamp = rospy.Time.now()
-        tf_msgs_base2odom.header.frame_id = 'odom'
-        tf_msgs_base2odom.child_frame_id = 'base_link'
-        tf_msgs_base2odom.transform.translation.x = 0
-        tf_msgs_base2odom.transform.translation.y = 0
-        tf_msgs_base2odom.transform.translation.z = 0
-        tf_msgs_base2odom.transform.rotation.x = 0
-        tf_msgs_base2odom.transform.rotation.y = 0
-        tf_msgs_base2odom.transform.rotation.z = 0
-        tf_msgs_base2odom.transform.rotation.w = 1
-        tf_broadcaster.sendTransform(tf_msgs_base2odom)
 
         rate.sleep()
